8cbbf3a833cd71d6a542375cd213f55cc79c479c
Mahesh Choudhary <mrak4123@gmail.com>
2022-01-16 23:49:42 -0800

    Flask program to mockAPI

---
+++ mock_weather.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 15 20:23:34 2018
+
+@author: Mahesh
+"""    
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/weather/2.5/data/')
+def list():
+   return render_template("weather.json")
+
+if __name__ == '__main__':
+   app.run(debug = True)
